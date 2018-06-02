bc6ec6128b62148cacfa0bac7ba8dad1951b1081
Alberto Biasao <biasao@gmail.com>
2018-06-02 17:23:56 -0300

    adding more comments to class snippet example.

--- class.js
+++ class.js
@@ -23,3 +23,6 @@ console.log(fluffykins.talk());
 console.log('Class in javascript is just a synthatic sugar for prototype model. ')
 console.log('hence Dog is just a function:', typeof Dog);
 console.log('and Dog is just the prototype of fluffykins ', Dog.prototype.isPrototypeOf(fluffykins));
+
+console.log('fluffykins delegates to Dog() on its __proto__:', fluffykins.__proto__)
+console.log('and also delegates to Mammal on the next hop in the prototype delegation chain: ', fluffykins.__proto__.__proto__)
